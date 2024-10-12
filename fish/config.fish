23d16d9d325e93503a4ff6c10f1e71edc2cdc4c2
Kevin Joseph <kevinjoseph1995@gmail.com>
2024-10-12 10:52:49 -0700

    Configs

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set PATH $HOME/.cargo/bin $PATH
+
+# Created by `pipx` on 2024-10-07 03:50:19
+set PATH $PATH /home/kevin/.local/bin
